377053e0f82cf1c122cb021431c8e30156805875
Davidnovicic <davidnovicic3@gmail.com>
2025-06-11 10:04:18 +0200

    changed next config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: { unoptimized: true },
-  output: "standalone",
+  output: "export",
+
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  engines: {
+    node: ">=15",
   },
 };
 
