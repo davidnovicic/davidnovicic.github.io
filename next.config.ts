2fc221a12a145e320492d3d649cc4f6a38098514
Davidnovicic <davidnovicic3@gmail.com>
2025-06-11 10:36:52 +0200

    change for Vercel deploy

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: { unoptimized: true },
-
+  output: "export",
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
